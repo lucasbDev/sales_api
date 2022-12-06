4f60b9647813846d25619365461e0849e558017e
lucasbDev <luccacpm@gmail.com>
2022-12-06 11:34:01 -0300

    build: prisma connection

---
+++ src/prisma/entities/customersEntity.ts
@@ -0,0 +1,7 @@
+export type CustomersEntity = {
+    id?: string
+    name: string
+    email: string
+    createdAt: Date | null
+    updateAt: Date | null
+}
---
+++ src/prisma/entities/usersEntity.ts
@@ -0,0 +1,8 @@
+export type UsersEntity = {
+    id?: string
+    name: string
+    password: string
+    avatar: string | null
+    createdAt: Date | null
+    updateAt: Date | null
+}
